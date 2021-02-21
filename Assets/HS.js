b7c77dab4cb30a7bd56333169fd02e4c7410157b
Andre Rodriguez <drae7299@gmail.com>
2021-02-21 01:07:44 -0500

    created event listened to clear scores upon choice, also created a way to pull local stored data to display the High schores

--- Assets/HS.js
+++ Assets/HS.js
@@ -1,3 +1,10 @@
 var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#clear");
 var goBack = document.querySelector("#goBack");
+
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
