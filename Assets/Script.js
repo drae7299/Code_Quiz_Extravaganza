8d5993848a6c77a3ae2bc0fd63f767333fdfcf88
Andre Rodriguez <drae7299@gmail.com>
2021-02-21 00:43:52 -0500

    created global vars

---
+++ Assets/Script.js
@@ -0,0 +1,6 @@
+var score = 0;
+var questionIndex = 0;
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
