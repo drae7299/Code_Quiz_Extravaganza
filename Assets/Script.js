01897d870bab70e02d50b55431983a51ddb86266
Andre Rodriguez <drae7299@gmail.com>
2021-02-21 00:52:57 -0500

    added in customized questions and respnses to pick, also added in the correct anser for the player to see if correct

--- Assets/Script.js
+++ Assets/Script.js
@@ -4,3 +4,32 @@ var currentTime = document.querySelector("#currentTime");
 var timer = document.querySelector("#startTime");
 var questionsDiv = document.querySelector("#questionsDiv");
 var wrapper = document.querySelector("#wrapper");
+
+var questions = [
+    {
+        title: "NORMALY used data types DO NOT include?",
+        choices: ["Strings","Booleans","Clickeroos","Numbers"],
+        answer: "CLickeroos"
+    },
+    {
+        title: "In a function, your If and Else statements MUST be contained ____?",
+        choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+        answer: "Curly Brackets"
+    },
+    {
+        title: "JavaScript Arrays can be used to hold... ____?",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables?",
+        choices: ["Semicolins", "Curly Brackets", "Quotes", "Parenthesis"],
+        answer: "Quotes"
+    },
+    {
+        title: "What can be used to Print out live information into your Console via Devtools Inspector?",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+
+];
